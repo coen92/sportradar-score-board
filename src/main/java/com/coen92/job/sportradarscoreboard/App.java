244aad4eae8948ff8d4ebe40decbe7101dda5062
coen92 <piotrkapica92@gmail.com>
2024-07-10 19:42:36 +0200

    SR-01: initial commit with test setup
    Spock Framework with Groovy test setup

---
+++ src/main/java/com/coen92/job/sportradarscoreboard/App.java
@@ -0,0 +1,13 @@
+package com.coen92.job.sportradarscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
